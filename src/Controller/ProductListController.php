828db7275d54d94e9591e52614d362772e1a7b84
Andrea Dolci <dolciandrea614@gmail.com>
2023-12-05 14:14:13 +0100

    sistemazioni varie

--- src/Controller/ProductListController.php
+++ src/Controller/ProductListController.php
@@ -2,6 +2,7 @@
     namespace App\Controller;
 
     use App\Service\ProductService;
+    use Symfony\Component\HttpFoundation\Request;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     
     class ProductListController extends AbstractController{
@@ -11,12 +12,45 @@
             $this -> product_service = $product_service;
         }
 
-        public function getMarketList(){
+        public function getProductList(Request $request){
+            $id = $request -> query -> get('id');
+            $productList;
 
-            $marketList = $this -> product_service -> getArray();
+            switch($id){
+                case 1: {
+                    $productList = $this -> product_service -> getFirst();
+                    break;
+                }
+                    
+                case 2: {
+                    $productList = $this -> product_service -> getSecond();
+                    break;
+                }
+                    
+                case 3: {
+                    $productList = $this -> product_service -> getThird();
+                    break;
+                }
+                    
+                case 4: {
+                    $productList = $this -> product_service -> getFourth();
+                    break;
+                }
+                    
+                case 5: {
+                    $productList = $this -> product_service -> getFifth();
+                    break;
+                }
+                    
+                case 6: {
+                    $productList = $this -> product_service -> getSixth();
+                    break;
+                }
+                    
+            }
             
-            return $this -> render('market-list.html.twig', [
-                'marketList' => $marketList
+            return $this -> render('product-list.html.twig', [
+                'productList' => $productList
             ]);
         }
     }
